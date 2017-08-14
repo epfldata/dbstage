95a8ff063480f4dac76a3c8d730412250f739c56
LPTK <lionel.parreaux@gmail.com>
2017-08-14 18:32:02 +0200

    RowFormat abstraction

--- src/main/scala/dbstage/frontend/Relation.scala
+++ src/main/scala/dbstage/frontend/Relation.scala
@@ -11,27 +11,35 @@ class Relation {
   protected val curColumns = mutable.ArrayBuffer[Column]()
   var data: Option[DataRep] = None
   
+  val table = Lazy {
+    val (keys,values) = columns.partition(_.isPrimary)
+    if (keys.isEmpty) Table(values)
+    //else PrimaryIndexedTable(keys, values)
+    else IndexedTable(keys, values, columns.map(_.name))
+  }
+  
   def columns: Seq[Column] = curColumns
   
   /** Partitions the columns into groups of columns that frequently accessed together and are thus to be stored in the same array. */
-  def columnsPartitioning: Seq[Row] = {
-    //???
-    // TODO
-    columns map (c => Row(c))
-  }
+  //def columnsPartitioning: Seq[Row] = {
+  //  //???
+  //  // TODO
+  //  columns map (c => Row(c))
+  //}
   
   //abstract class Column(name: String) extends Field(name) {
-  abstract case class Column(override val name: String) extends Field(name) {
+  abstract case class Column(override val name: String, val isPrimary: Bool) extends Field(name) {
     //val name: String
     //type T
     //implicit val IRTypeT: IRType[T]
     //implicit val SerialT: Serial[T]
+    assert(!table.computed)
     curColumns += this
   }
   object Column {
     //def apply[T](name: String, foreignIn: Table = null) = {
     def apply[T0:IRType:Serial](name: String, primary: Bool = false, foreign: Relation # Column = null) = {
-      new Column(name) {
+      new Column(name,primary) {
         //val name = 
         type T = T0
         val IRTypeT: IRType[T] = implicitly
@@ -48,10 +56,16 @@ class Relation {
     
     
     //println(Table(columns))
-    val tbl = Table(columns)
+    //val tbl = Table(columns)
+    val tbl = table.value
     //println(tbl.parse)
     //println(tbl.load)
     
+    tbl.loadData(src.getLines())
+    
+    //println(tbl.show)
+    
+    /*
     val sep = '|'
     val arr: Code[Array[String]] = ir"arr?:Array[String]"
     val parser = tbl.parse(columns.zipWithIndex.map(ci => ci._1.name -> ir"$arr(${Const(ci._2)})").toMap)
@@ -72,6 +86,8 @@ class Relation {
     
     //println(tbl.buffer)
     println(tbl.show)
+    */
+    
     
   }
   
@@ -84,7 +100,7 @@ class Relation {
 trait TableQuery {
   type ValueQuery = Code[Bool]
   
-  val row: Row
+  //val row: Row
   
   def project(fields: Field*): TableQuery = ???
   def filter(pred: ValueQuery): TableQuery = ???
@@ -93,17 +109,17 @@ trait TableQuery {
   
 }
 
-abstract class Row {
-  type T <: Product
-  implicit val T: IRType[T]
-  //val typs: Seq[IRType[_]]
-  val fields: Seq[Field]
-  //def parse(raw: Iterator[String]): T
-  def parse: Code[Iterator[String]] => Code[T]
-}
-object Row {
-  def apply(fs: Field*): Row = ???
-}
+//abstract class Row {
+//  type T <: Product
+//  implicit val T: IRType[T]
+//  //val typs: Seq[IRType[_]]
+//  val fields: Seq[Field]
+//  //def parse(raw: Iterator[String]): T
+//  def parse: Code[Iterator[String]] => Code[T]
+//}
+//object Row {
+//  def apply(fs: Field*): Row = ???
+//}
 
 
 
--- src/main/scala/dbstage/runtime/DataRep.scala
+++ src/main/scala/dbstage/runtime/DataRep.scala
@@ -44,6 +44,110 @@ object InMemoryStorageManager extends StorageManager {
   
 }
 
+trait RowFormat {
+  type Repr
+  implicit val Repr: IRType[Repr]
+  val columns: Seq[Field]
+  val parse: Map[String, Code[String]] => Code[Repr]
+}
+object RowFormat {
+  def apply(cols: Seq[Field]): RowFormat = {
+    val size = cols.size
+    if (size == 1) new SingleColumnFormat(cols.head)
+    else if (size > 22) {
+      val t = cols.take(size/2)
+      CompositeFormat(RowFormat(t), RowFormat(cols.drop(t.size)))
+    }
+    else new TupleFormat(cols)
+  }
+}
+class SingleColumnFormat(val col: Field) extends RowFormat {
+  type Repr = col.T
+  implicit val Repr = col.IRTypeT // Note: removing `implicit` creates a compiler crash...
+  val columns = col :: Nil
+  val parse = (cs: Map[String, Code[String]]) => ir"${col.SerialT.parse}(${cs(col.name)})"
+}
+//class TupleFormat(val columns: Seq[Field]) extends RowFormat {
+case class TupleFormat(columns: Seq[Field]) extends RowFormat {
+  val size = columns.size
+  val clsSym = base.loadTypSymbol(s"scala.Tuple${size}")
+  val objSym = base.loadTypSymbol(s"scala.Tuple${size}$$")
+  val mtd = base.loadMtdSymbol(objSym, "apply", None)
+  val typs = columns.map(_.IRTypeT.rep).toList
+  val typ = base.staticTypeApp(clsSym, typs)
+  implicit val Repr = base.IRType[Repr](typ)
+  val parse = {
+    import base._
+    (cs: Map[String, Code[String]]) => {
+      IR(methodApp(staticModule(s"scala.Tuple${size}"), mtd, typs, Args(columns map (c => 
+        ir"${c.SerialT.parse}(${cs(c.name)})".rep): _*)::Nil, typ))
+    }
+  }
+}
+case class CompositeFormat(val lhs: RowFormat, val rhs: RowFormat) extends RowFormat {
+  import lhs.{Repr=>LR}, rhs.{Repr=>RR}
+  type Repr = (lhs.Repr, rhs.Repr)
+  //val Repr = irTypeOf[(lhs.Repr,rhs.Repr)]
+  lazy val Repr = irTypeOf[(LR,RR)]
+  val columns = lhs.columns ++ rhs.columns
+  val parse = (cs: Map[String, Code[String]]) => ir"(${lhs.parse(cs)}, ${rhs.parse(cs)})"
+}
+
+//abstract class Table(val rf: RowFormat) {
+trait Table {
+  val rowFmt: RowFormat
+  def mkDataLoader(sep: Char): CrossStage[Iterator[String] => Unit]
+  def loadData(data: Iterator[String], sep: Char = '|'): Unit = {
+    val pgrm = mkDataLoader(sep)
+    println(s"Generated Program: $pgrm")
+    pgrm.compile()(data)
+  }
+}
+object Table {
+  def apply(cols: Seq[Field]): Table = new PlainTable(cols)
+}
+class PlainTable(val cols: Seq[Field]) extends Table {
+  val rowFmt: RowFormat = TupleFormat(cols)
+  //def loadData(data: Iterator[String], sep: Char = '|'): CrossStage[Unit] = ???
+  def mkDataLoader(sep: Char): CrossStage[Iterator[String] => Unit] = ???
+}
+//class IndexedTable(val keys: Seq[RowFormat], val values: Seq[RowFormat]) extends Table {
+case class IndexedTable(keys: Seq[Field], values: Seq[Field], order: Seq[String]) extends Table {
+  val keyFmt: RowFormat = RowFormat(keys) // Note: using TupleFormat here may generate references to Tuple1... not sure how that's handled by Scala
+  val valFmt: RowFormat = RowFormat(values)
+  val rowFmt: RowFormat = CompositeFormat(keyFmt,valFmt)
+  
+  //type Key = keyFmt.Repr
+  //type Val = valFmt.Repr
+  import keyFmt.{Repr=>Key}
+  import valFmt.{Repr=>Val}
+  
+  val hashTable = mutable.HashMap[Key,Val]()
+  
+  //def loadData(data: Iterator[String], sep: Char = '|'): CrossStage[Unit] = {
+  def mkDataLoader(sep: Char): CrossStage[Iterator[String] => Unit] = {
+    val arr: Code[Array[String]] = ir"arr?:Array[String]"
+    //val colMap = columns.zipWithIndex.map(ci => ci._1.name -> ir"$arr(${Const(ci._2)})").toMap
+    val colMap = order.zipWithIndex.toMap.mapValues(name => ir"$arr(${Const(name)})")
+    val kparser = keyFmt.parse(colMap)
+    val vparser = valFmt.parse(colMap)
+    //val kparser = keyFmt.parse(keys.zipWithIndex.map(ci => ci._1.name -> ir"$arr(${Const(ci._2)})").toMap)
+    //val vparser = valFmt.parse(values.zipWithIndex.map(ci => ci._1.name -> ir"$arr(${Const(ci._2)})").toMap)
+    (CrossStage(hashTable) { ht => 
+      ir"""(ite: Iterator[String]) =>
+        while (ite.hasNext) {
+          val str = ite.next
+          val arr = str.split(${Const(sep)})
+          //println(">"+arr.toList)
+          $ht += (${kparser:IR[Key,{val arr:Array[String]}]} -> ${vparser:IR[Val,{val arr:Array[String]}]})
+        }
+      """
+    })//(dbg.implicitType[mutable.HashMap[Key,Val]],irTypeOf[Iterator[String] => Unit])
+  }
+  
+}
+
+/*
 // TODO Table stored as hashmaps, compressed arrays of bits, etc
 //abstract class Table[Row:IRType] {
 trait Table {
@@ -75,7 +179,7 @@ object Table {
     if (size == 1) SingleColumnTable(cols.head)
     else if (size > 22) {
       val t = cols.take(size/2)
-      new CompositeTable(Table(t), Table(cols.drop(t.size)))
+      CompositeOf(Table(t), Table(cols.drop(t.size)))
     }
     else {
       val clsSym = base.loadTypSymbol(s"scala.Tuple${size}")
@@ -86,7 +190,7 @@ object Table {
       //println(clsSym,objSym,mtd,typ)
       
       //new Table[Any]()(base.IRType(typ))
-      new Table
+      new Table // extract as PlainTupleTable
       {
         implicit val Row = base.IRType[Row](typ)
         val buffer = mutable.ArrayBuffer[Row]()
@@ -121,29 +225,52 @@ object Table {
 /*class CompositeTable[T0:IRType,T1:IRType](lhs: Table[T0], rhs: Table[T1]) extends Table[(T0,T1)] {
   
 }*/
-class CompositeTable(val lhs: Table, val rhs: Table) extends Table {
+//class CompositeTable(val lhs: Table, val rhs: Table) extends Table {
+trait CompositeTable extends Table {
+  val lhs: Table
+  val rhs: Table
   //import lhs.{Row}, rhs.{Row}
   import lhs.{Row=>LR}, rhs.{Row=>RR}
   type Row = (lhs.Row, rhs.Row)
   //val Row = irTypeOf[(lhs.Row,rhs.Row)]
-  val Row = irTypeOf[(LR,RR)]
+  lazy val Row = irTypeOf[(LR,RR)]
+}
+case class CompositeOf(val lhs: Table, val rhs: Table) extends CompositeTable
+//object CompositeTable {
+//  def apply(lhs: Table, rhs: Table) = new CompositeTable {
+//    
+//  }
+//}
+class SingleColumnTable[T:IRType] extends Table {
+  type Row = T
+  val Row = implicitly[IRType[T]]
 }
 object SingleColumnTable {
   val IntType = irTypeOf[Int]
   //def apply(col: Field): Table[_] = {
   def apply(col: Field): Table = {
     col.IRTypeT match {
-      case IntType => ??? // TODO compressed column?
-      case _ => ??? // TODO
+      //case IntType => ??? // TODO compressed column?
+      case _ => new SingleColumnTable[col.T]
     }
   }
 }
-/*
-abstract class IndexedTable[T:IRType](val keys: Seq[Field], val cols: Seq[Field]) extends Table[T] {
+abstract class IndexedTable(val keys: Seq[Field], val values: Seq[Field]) extends Table {
   
 }
+case class PrimaryIndexedTable(primaryKeys: Seq[Field], override val values: Seq[Field]) extends IndexedTable(primaryKeys, values) with CompositeTable {
+  val cols = primaryKeys ++ values
+  val lhs = Table(primaryKeys)
+  val rhs = Table(values)
+}
+//object PrimaryIndexedTable {
+//  def apply(primaryKeys: Seq[Field], cols: Seq[Field])
+//}
+
+/*
+abstract class IndexedTable[T:IRType](val keys: Seq[Field], val cols: Seq[Field]) extends Table[T] {
+}
 abstract class PrimaryIndexedTable[T:IRType](primaryKeys: Seq[Field], cols: Seq[Field]) extends IndexedTable[T](primaryKeys, cols) {
-  
 }
 */
 
@@ -151,6 +278,7 @@ abstract class PrimaryIndexedTable[T:IRType](primaryKeys: Seq[Field], cols: Seq[
 // TODO:
 //class CompressedTable extends Table[Int] {
 //}
+*/
 
 
 
