b9693c851bb42b874e3699ce24beda2326a679bc
LPTK <lionel.parreaux@gmail.com>
2018-06-02 16:27:25 +0200

    Make Vector type-generic

--- src/main/scala/gen/example/MyApp.scala
+++ src/main/scala/gen/example/MyApp.scala
@@ -8,12 +8,16 @@ import Embedding.Quasicodes.{code=>c,_}
 object MyApp extends Embedding.ProgramGen with Vectors with App {
   val root = Root.apply
   
-  val V3 = Vector(3)
-  import V3.{Self => V3T}
+  val V3 = Vector[Int](3)
   println(V3.showCode)
+  import V3.{Self => V3T}
   //println(c{(v: V3T) => $(V3.xs(1))(v)+1})
   //println(c"(v: V3T) => ${V3.xs(0)}(v)+1")
   //println(V3.prod.inlined)
+  
+  val VD4 = Vector[Double](4)
+  
+  println(VD4.showCode)
   
   println("Done.")
   
--- src/main/scala/gen/example/Vectors.scala
+++ src/main/scala/gen/example/Vectors.scala
@@ -6,17 +6,40 @@ import dbstage.Embedding
 import Embedding.Predef._
 import Embedding.Quasicodes.{code=>c,_}
 
+trait Num[T] {
+  def zero: ClosedCode[T]
+  def plus: ClosedCode[(T,T) => T]
+  def prod: ClosedCode[(T,T) => T]
+}
+object Num {
+  def apply[N: Num]: Num[N] = implicitly
+  
+  implicit object NumInt extends Num[Int] {
+    def zero = c"0"
+    def plus = c"(_:Int) + (_:Int)"
+    def prod = c"(_:Int) * (_:Int)"
+  }
+  implicit object NumDouble extends Num[Double] {
+    def zero = c"0.0"
+    def plus = c"(_:Double) + (_:Double)"
+    def prod = c"(_:Double) * (_:Double)"
+  }
+}
+
 trait Vectors extends Embedding.ProgramGen {
   
-  class Vector(n: Int) extends Class()("Vector"+n) {
-    val xs = Array.fill(n)(param[Int])
+  class Vector[N: CodeType: Num](n: Int) extends Class()(s"Vector_${n}_${typeRepOf[N]}") {
+    val N = Num[N]
+    val xs = Array.fill(n)(param[N])
     //val xs = Array.fill(n)(param[Int]).lift.andThen(_.getOrElse(lastWords("Out of bounds!")))  // not a Seq; can't iterate!
     //val prod = method(c"(that: Self) => ${xs.foldLeft(c"0" withContextOf that)((r,x) => c"$r + $x * ${x.toLambda}(?that:Self)")}")
     val prod = method {
       val that = Variable[Self]
-      c"{ ($that) => ${xs.foldLeft[C[Int, Ctx & that.Ctx]](c"0")((r,x) => c"$r + $x * ${x.asLambda}($that)")} }"
+      //c"{ ($that) => ${xs.foldLeft[C[Int, Ctx & that.Ctx]](c"0")((r,x) => c"$r + $x * ${x.asLambda}($that)")} }"
+      c"{ ($that) => ${xs.foldLeft[C[N, Ctx & that.Ctx]](N.zero)((r,x) => c"${N.plus}($r, ${N.prod}($x, ${x.asLambda}($that)))")} }"
+      // ^ would be nice to somehow allow syntax sugar so c"a + b" means c"${N.plus}(a, b)"
     }
   }
-  def Vector(n: Int) = new Vector(n: Int)
+  def Vector[N: CodeType: Num](n: Int) = new Vector[N](n: Int)
   
 }
