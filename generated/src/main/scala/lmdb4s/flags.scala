1ecf8585150409a8d3c165e099a31d6072362d0b
Lucien Iseli <lucien.iseli93@gmail.com>
2020-04-30 14:23:31 +0200

    Add LMDB env flags

--- generated/src/main/scala/lmdb4s/flags.scala
+++ generated/src/main/scala/lmdb4s/flags.scala
@@ -1,7 +1,20 @@
 package lmdb4s
 
 object flags {
-  /* --- Flags for Dbi functions --- */
+  /* --- Environment flags --- */
+  val MDB_FIXEDMAP: Int = 0x01
+  val MDB_NOSUBDIR: Int = 0x4000
+  val MDB_NOSYNC: Int = 0x10000
+  val MDB_RDONLY: Int = 0x20000
+  val MDB_NOMETASYNC: Int = 0x40000
+  val MDB_WRITEMAP: Int = 0x80000
+  val MDB_MAPASYNC: Int = 0x100000
+  val MDB_NOTLS: Int = 0x200000
+  val MDB_NOLOCK: Int = 0x400000
+  val MDB_NORDAHEAD: Int = 0x800000
+  val MDB_NOMEMINIT: Int = 0x1000000
+
+  /* --- Database flags --- */
   val MDB_REVERSEKEY: Int = 0x02
   val MDB_DUPSORT: Int = 0x04
   val MDB_INTEGERKEY: Int = 0x08
